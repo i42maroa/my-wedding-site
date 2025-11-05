6cfc243c56ac5a15e5c16e52e23dd103d12432ad
Antonio <amrodriguezjarote@gmail.com>
2025-11-05 13:51:05 +0100

    Wip

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import SectionDate from "@/components/sections/date/SectionDate";
 import SectionVehicle from "@/components/sections/vehicle/SectionVehicle";
 import SectionSleep from "@/components/sections/sleep/SectionSleep";
 import MainLayout from "@/components/layout/main/MainLayout";
+import SectionCeremony from "@/components/sections/ceremony/SectionCeremony";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <MainLayout>
         <SectionHeader></SectionHeader>
         <SectionDate></SectionDate>
+        <SectionCeremony></SectionCeremony>
         <SectionVehicle></SectionVehicle>
         <SectionSleep></SectionSleep>
       </MainLayout>
--- components/sections/vehicle/SectionVehicle.tsx
+++ components/sections/vehicle/SectionVehicle.tsx
@@ -1,23 +1,27 @@
-import styles from "./SectionVehicle.module.css";
 import ExternalLinkButton from "@/components/button/ExternalLinkButton";
-import FloralLayout from "@/components/layout/floral/FloralLayout";
+import styles from "./SectionVehicle.module.css";
 
 export default function SectionVehicle() {
   return (
     <section className={styles.container}>
-      <FloralLayout className={styles.layout}>
-            <h2>Ceremonia y celebración</h2>
-            <p>19:00h Centro lúdico de Belmez</p>
-            <div className={styles.element}>
-                <h3>Autobús</h3>
-                <p>Vva de Córdoba - Peñarroya - Belmez</p>
-                <p>(Pendiente de confirmación)</p>
-            </div>
+            <h2>Transporte</h2>
             <div className={styles.element}>
                 <h3>Coche</h3>
+                <p>En el lugar hay un amplio lugar de aparcamiento.</p>
                 <ExternalLinkButton className={styles.button} href='https://maps.app.goo.gl/8jQSVLh2XEj8Uhbv8'>Cómo llegar</ExternalLinkButton>
+
             </div>
-      </FloralLayout>
+            <div className={styles.element}>
+                <h3>Autobús</h3>
+
+                <p>- Salida desde Villanueva de Córdoba (hora pendiente de confirmar)</p>
+                <p>- Paso por Peñarroya</p>
+                <p>- LLegada al Centro Lúdico</p>
+
+                <p className={styles.bold}>(Si quieres ir en el autobús debes indicarlo en la confirmación de la boda)</p>
+
+            </div>
+            
     </section>
   );
 }
