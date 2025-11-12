ee681a5eeebc4516544092924bcb5c247fd7ce40
Antonio <amrodriguezjarote@gmail.com>
2025-11-12 21:00:40 +0100

    add map of cordoba

--- components/sections/vehicle/SectionVehicle.tsx
+++ components/sections/vehicle/SectionVehicle.tsx
@@ -1,21 +1,19 @@
+import SvgMap from "@/components/svg/SvgMap/SvgMap";
 import styles from "./SectionVehicle.module.css";
 
 export default function SectionVehicle() {
   return (
     <section id="vehicle" className={styles.container}>
-            <h2>Transporte</h2>
-            <div className={styles.element}>
-                <h3>Autobús</h3>
-                <p>- Salida desde Villanueva de Córdoba (hora pendiente de confirmar)</p>
-                <p>- Paso por Peñarroya</p>
-                <p>- LLegada al Centro Lúdico</p>
-                <p className={styles.bold}>(Si quieres ir en el autobús debes indicarlo en la confirmación de la boda)</p>
-            </div>
-            <div className={styles.element}>
-                <h3>Coche</h3>
-                <p>En el lugar hay un amplio lugar de aparcamiento.</p>
-            </div>
-            
+            <h2>Consejos de Viaje</h2>
+
+            <div className={styles.contentContainer}>
+              <SvgMap/>
+              <div className={styles.content}>
+                  <h3>Cómo llegar a Belmez</h3>
+                  <p>Habrá autobús con salida desde Villanueva de Córdoba, pasará por Peñarroya y llegará justo antes del comienzo de la ceremonia. (hora pendiente de confirmar).</p>
+                  <p>Si no quieres autobús, siempre puedes ir en tu propio coche.</p>
+              </div>           
+            </div>                      
     </section>
   );
 }
