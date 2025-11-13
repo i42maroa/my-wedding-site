bb3e8634689d8cbf3d890221052c370b1e085098
Antonio <amrodriguezjarote@gmail.com>
2025-11-13 14:01:29 +0100

    fix header

--- components/header/SectionHeader.tsx
+++ components/header/SectionHeader.tsx
@@ -5,13 +5,12 @@ export default function SectionHeader() {
   return (
   <div className={styles.container}>
       <div className={styles.flowerContainer}>
-        <Image className={styles.flower1} src={'/frontal-flower.webp'} alt="Los novios" fill priority sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 70vw"/>
+        <Image className={styles.flower1} src={'/frontal-flower.webp'} alt="Los novios" fill priority />
       </div>
       <h1>Antonio & Sheila</h1>
       <h2>Nuestra boda</h2>
       <div className={styles.flowerContainer}>
-        <Image className={styles.flower2} src={'/frontal-flower.webp'} alt="Los novios" fill priority sizes="(max-width: 768px) 100vw,
-           (max-width: 1200px) 80vw, 70vw"/>
+        <Image className={styles.flower2} src={'/frontal-flower.webp'} alt="Los novios" fill priority />
       </div>
    </div>
   );
