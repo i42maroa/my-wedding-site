11839de69806e83e7561e601aa733a2a4fc6e21d
Antonio <amrodriguezjarote@gmail.com>
2025-11-18 17:54:30 +0100

    fix searchParams issue

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import styles from "./Form.module.css"
 import FormButton from "@/components/button/FormButton";
 import RadioButton from "@/components/form/radio-button/RadioButton";
@@ -97,109 +97,111 @@ export default function RSVPPage() {
 
 
   return (
-    <MainLayout header={false}>
-    <FloralLayout>
-    <div className={styles.container}>
-      <h2 className={styles.title}>Asistencia</h2>
-      <form className={styles.formContainer} onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <p>Hola, { names.length > 1 ? 'somos ': 'soy '}
-           {
-            isFormWithAccessCode(id) && (
-              <>
-                <span className={styles.names}>{ names.length === 1 ? names[0] : names.slice(0, -1).join(', ') + ' y ' + names[names.length - 1]}</span>
-                <span> y { names.length > 1 ? 'confirmamos': 'confirmo'} la asistencia a vuestra boda el día:</span>
-              </>
-            )} 
-           </p>      
-          {
-            !isFormWithAccessCode(id) && (
-              <>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <MainLayout header={false}>
+      <FloralLayout>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Asistencia</h2>
+        <form className={styles.formContainer} onSubmit={handleSubmit}>
+          <div className={styles.formGroup}>
+            <p>Hola, { names.length > 1 ? 'somos ': 'soy '}
+            {
+              isFormWithAccessCode(id) && (
+                <>
+                  <span className={styles.names}>{ names.length === 1 ? names[0] : names.slice(0, -1).join(', ') + ' y ' + names[names.length - 1]}</span>
+                  <span> y { names.length > 1 ? 'confirmamos': 'confirmo'} la asistencia a vuestra boda el día:</span>
+                </>
+              )} 
+            </p>      
+            {
+              !isFormWithAccessCode(id) && (
+                <>
+                <FormInput
+                  name="nombre"
+                  label="Indica vuestros nombres"
+                  placeholder="Ej: Ana García Rosales, Jose María Martinez"
+                  value={formData.nombre!}
+                  onChange={handleInputChange}
+                  required
+                  error={errors.nombre}/>
+                  <p>y { names.length > 1 ? 'confirmamos': 'confirmo'} la asistencia a vuestra boda el día</p>
+                </>
+              ) 
+            }
+              
+            <h3 className={styles.date}>22 de Agosto de 2026</h3>
+          </div>
+          
+          <div className={styles.formGroup}>
+            <p>Respecto a la comida:</p>
+            <div className={styles.radioButtonContainer}>
+              <RadioButton
+                name="intolerancia"
+                value={false}
+                label="No padezco/padecemos ninguna intolerancia"
+                selectedValue={formData.intolerancia}
+                onChange={() => setFormData({ ...formData, intolerancia: false })}
+              />
+              <RadioButton
+                name="intolerancia"
+                value={true}
+                label="Sí, tengo alguna intolerancia"
+                selectedValue={formData.intolerancia}
+                onChange={() => setFormData({ ...formData, intolerancia: true })}
+              /> 
+            </div>
+
+            {formData.intolerancia && (
               <FormInput
-                name="nombre"
-                label="Indica vuestros nombres"
-                placeholder="Ej: Ana García Rosales, Jose María Martinez"
-                value={formData.nombre!}
+                name="detallesIntolerancia"
+                label="Indica tus intolerancias"
+                placeholder="Ej: gluten, lactosa...)"
+                value={formData.detallesIntolerancia}
                 onChange={handleInputChange}
+                error={errors.detallesIntolerancia}
                 required
-                error={errors.nombre}/>
-                <p>y { names.length > 1 ? 'confirmamos': 'confirmo'} la asistencia a vuestra boda el día</p>
-              </>
-            ) 
-          }
-             
-          <h3 className={styles.date}>22 de Agosto de 2026</h3>
-        </div>
-        
-        <div className={styles.formGroup}>
-          <p>Respecto a la comida:</p>
-          <div className={styles.radioButtonContainer}>
-            <RadioButton
-              name="intolerancia"
-              value={false}
-              label="No padezco/padecemos ninguna intolerancia"
-              selectedValue={formData.intolerancia}
-               onChange={() => setFormData({ ...formData, intolerancia: false })}
-            />
-            <RadioButton
-              name="intolerancia"
-              value={true}
-              label="Sí, tengo alguna intolerancia"
-              selectedValue={formData.intolerancia}
-               onChange={() => setFormData({ ...formData, intolerancia: true })}
-            /> 
+              />
+            )}
           </div>
+          <div className={styles.formGroup}>
+            <p>Respecto a cómo iré a la boda:</p>
+            <div className={styles.radioButtonContainer}>
+              <RadioButton
+                name="transporte"
+                value="car"
+                label="Voy en coche"
+                selectedValue={formData.transporte}
+                onChange={() => setFormData({ ...formData, transporte: 'car' })}
+              />
 
-          {formData.intolerancia && (
-            <FormInput
-              name="detallesIntolerancia"
-              label="Indica tus intolerancias"
-              placeholder="Ej: gluten, lactosa...)"
-              value={formData.detallesIntolerancia}
-              onChange={handleInputChange}
-              error={errors.detallesIntolerancia}
-              required
-            />
-          )}
-        </div>
-        <div className={styles.formGroup}>
-          <p>Respecto a cómo iré a la boda:</p>
-          <div className={styles.radioButtonContainer}>
-            <RadioButton
-              name="transporte"
-              value="car"
-              label="Voy en coche"
-              selectedValue={formData.transporte}
-              onChange={() => setFormData({ ...formData, transporte: 'car' })}
-            />
-
-            <RadioButton
-              name="transporte"
-              value="bus"
-              label="Voy en autobús"
-              selectedValue={formData.transporte}
-              onChange={() => setFormData({ ...formData, transporte: 'bus' })}
-            />
+              <RadioButton
+                name="transporte"
+                value="bus"
+                label="Voy en autobús"
+                selectedValue={formData.transporte}
+                onChange={() => setFormData({ ...formData, transporte: 'bus' })}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.formGroup}>
-          <p>Además, quiero indicar:</p>
-            <FormInput
-              name="mensaje"
-              label="Mensaje para los novios (opcional)"
-              placeholder="Ej: Me "
-              value={formData.mensaje}
-              onChange={handleInputChange}
-            />  
-        </div>      
-        <FormButton className={styles.button}
-                type="submit" 
-                // disabled={loading} TODO: BLOCK WHEN IS LOADING
-                >Enviar
-        </FormButton>
-      </form>
-    </div>
-    </FloralLayout>
-    </MainLayout>
+          <div className={styles.formGroup}>
+            <p>Además, quiero indicar:</p>
+              <FormInput
+                name="mensaje"
+                label="Mensaje para los novios (opcional)"
+                placeholder="Ej: Me "
+                value={formData.mensaje}
+                onChange={handleInputChange}
+              />  
+          </div>      
+          <FormButton className={styles.button}
+                  type="submit" 
+                  // disabled={loading} TODO: BLOCK WHEN IS LOADING
+                  >Enviar
+          </FormButton>
+        </form>
+      </div>
+      </FloralLayout>
+      </MainLayout>
+    </Suspense>
   );
 }
