fab5d5613fcc701fdd8a7a499b2e94f3a1ea96e1
Antonio <amrodriguezjarote@gmail.com>
2026-01-13 18:41:31 +0100

    add animation styles

--- components/layout/floral/FloralLayout.tsx
+++ components/layout/floral/FloralLayout.tsx
@@ -14,13 +14,11 @@ export default function FloralLayout ({children, className }:FloralLayoutProps) 
     <div className={`${styles.container} ${className}`}>
       <div className={`${styles.imageContainer} ${styles.topLeft}`}>
         <Image className={`${styles.flower}`} src={'/flower1.webp'} alt="Flor decorativa superior izquierda" priority fill sizes="(max-width: 768px) 88vw,
-         (max-width: 1200px) 80vw,
-         66vw"/>
+         (max-width: 1200px) 80vw, 66vw"/>
       </div>
       <div className={`${styles.imageContainer} ${styles.bottomRight}`}>
         <Image className={`${styles.flower}`} src={'/flower2.webp'} alt="Flor decorativa inferior derecha" priority fill sizes="(max-width: 768px) 88vw,
-         (max-width: 1200px) 80vw,
-         66vw"/>
+         (max-width: 1200px) 80vw, 66vw"/>
       </div>
       <div className={styles.content}>
         {children}
--- components/sections/vehicle/SectionVehicle.tsx
+++ components/sections/vehicle/SectionVehicle.tsx
@@ -5,13 +5,12 @@ import ExternalLinkButton from "@/components/button/ExternalLinkButton";
 export default function SectionVehicle() {
   return (
     <section id="vehicle" className={styles.container}>
-            <h2>Consejos de Viaje</h2>
+            <h2>Cómo llegar a Belmez</h2>
 
             <div className={styles.contentContainer}>
               <SvgMap className={styles.map}/>
               <div className={styles.content}>
-                  <h3>Cómo llegar a Belmez</h3>
-                  <p>Habrá autobús con salida desde Villanueva de Córdoba, pasará por Peñarroya y llegará justo antes del comienzo de la ceremonia. (hora pendiente de confirmar).</p>
+                  <p>Habrá autobús con salida desde Villanueva de Córdoba, pasará por Peñarroya y llegará justo antes del comienzo de la ceremonia. (hora de salida pendiente de confirmar).</p>
                   <p>Si no quieres autobús, siempre puedes ir en tu propio coche.</p>
                   <ExternalLinkButton className={styles.button} href='https://maps.app.goo.gl/8jQSVLh2XEj8Uhbv8'>Cómo llegar</ExternalLinkButton>
               </div>           
